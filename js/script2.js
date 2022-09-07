76969af2b67e83523a98dcd983ec37224df8d05b
ecoatelant <emiliecoatelant@outlook.fr>
2022-09-08 00:46:39 +0200

    Dernier commit avant soutenance

--- js/script2.js
+++ js/script2.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     $(".item").click(function(){
         $(".view-project .description").html("Nouveau texte");
-        if($(this).hasClass("vegandco")){
+        if($(this).hasClass("schoolapps")){
+            $(".view-project .description").load("project_schoolapps.html");
+            $(".view-project .capture").html("<img src='/img/schoolapps.png'>");
+        }else if($(this).hasClass("vegandco")){
             $(".view-project .description").load("project_vegandco.html");
+            $(".view-project .capture").html('<img src="img/vegandco.png">');
+        }else if($(this).hasClass("pot2colle")){
+            $(".view-project .description").load("project_pot2colle.html");
+            $(".view-project .capture").html('<img src="img/refonte_pot2colle.png">');;
         }
     });
 });
